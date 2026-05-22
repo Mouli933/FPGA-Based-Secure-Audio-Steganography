099553cee136f37d9811d9eae4276b241726aa1d
Mouli933 <moulithota372@gmail.com>
2026-05-22 12:03:01 +0530

    Add testbench for top_audio_pwm module

--- Verilog/tb.v
+++ Verilog/tb.v
@@ -1,1 +1,28 @@
 
+module tb;
+
+reg clk = 0;
+always #10 clk = ~clk;
+
+reg [3:0] key;
+
+wire outL, outR;
+
+top_audio_pwm uut (
+    .clk50(clk),
+    .key(key),
+    .AUDIO_OUT_L(outL),
+    .AUDIO_OUT_R(outR)
+);
+
+initial begin
+    key = 4'b0000;   // wrong key → tune
+    #100000;
+
+    key = 4'b1010;   // correct key → secret
+    #100000;
+
+    $stop;
+end
+
+endmodule
